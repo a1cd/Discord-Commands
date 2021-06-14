5a9d7466ceb6b50ca17d8ff95c22edfe531bbd4d
a1cd <71281043+a1cd@users.noreply.github.com>
2021-06-14 18:47:43 -0400

    Stabilized and fixed !uptime Command!

--- index.js
+++ index.js
@@ -100,9 +100,35 @@ function help(inp, msg, cmd) {
  * @param {Cmd} cmd 
  */
 function uptime(inp, msg, cmd) {
-  let tseconds = Math.floor(bot.uptime/1000)
-  let tmiliseconds = bot.uptime-(tseconds*1000)
-  msg.reply("i have been up for "+tseconds.toString()+"."+tmiliseconds.toString())
+  function msToTime(time) {
+    function Reformat(Rounding, places, time) {
+      let tseconds1 = Math.floor(time / Rounding);
+      let tmiliseconds = time - (tseconds1 * Rounding);
+      let tmiliString = tmiliseconds.toString();
+      let milistr1 = tmiliString.padStart(places, "0");
+      return { tseconds1, milistr1 };
+    }
+    
+    let { tseconds1:tseconds, milistr1:milistr } = Reformat(1000, 3, time);
+    let { tseconds1:tminuites, milistr1:secstr } = Reformat(60, 1, tseconds);
+    let { tseconds1:thours, milistr1:minstr } = Reformat(60, 1, tminuites);
+    let { tseconds1:tdays, milistr1:minhour } = Reformat(24, 1, thours);
+    var timeList = []
+    var nameList = []
+    for (let i = 0; i < [[minhour, "hour"], [minstr, "min"], [(secstr+"."+milistr), ("sec.MS")]].length; i++) {
+      const thing = [[minhour, "hour"], [minstr, "min"], [(secstr+"."+milistr), ("sec.MS")]][i];
+      if (thing[0] != 0) {
+        timeList.push(thing[0])
+        nameList.push(thing[1])
+      }
+    }
+    if (tdays == 0) {
+      return (timeList.join(":")+" ("+nameList.join(":")+")")
+    } else {
+      return (tdays.toString()+" days and "+timeList.join(":")+" ("+nameList.join(":")+")")
+    }
+  }
+  msg.reply("i have been up for "+msToTime(bot.uptime))
 }
 function addRule(inp, msg, cmd) {
   msg.reply("yep")
