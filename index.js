b329e61d52b9dd37a73c9c5c67a8b629298d7f6e
a1cd <71281043+a1cd@users.noreply.github.com>
2021-06-16 18:30:20 -0400

    Shrinked it and added exports

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-require('dotenv').config();
 const Discord = require('discord.js');
-const mongo = require('mongodb');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN
-
-const MongoClient = require('mongodb').MongoClient;
-
-const test = require('assert');
-
-// Connection url
-
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-
-const dbName = 'discord';
-
-// Connect using MongoClient
-
-bot.login(TOKEN)
-
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
-});
+module.exports = {
+  Cmd,
+  help
+}
 class Cmd {
   /**
    * @type {String}
@@ -92,15 +73,6 @@ class Cmd {
   }
 }
 /**
- * 
- * @param {String} inp 
- * @param {Discord.Message} msg 
- * @param {Cmd} cmd 
- */
-function ping(inp, msg, cmd) {
-  msg.reply("pong")
-}
-/**
  * @param {String} inp 
  * @param {Discord.Message} msg 
  * @param {Cmd} cmd 
@@ -113,77 +85,34 @@ function help(inp, msg, cmd) {
  * @param {Discord.Message} msg 
  * @param {Cmd} cmd 
  */
-function uptime(inp, msg, cmd) {
-  function msToTime(time) {
-    function Reformat(Rounding, places, time) {
-      let tseconds1 = Math.floor(time / Rounding);
-      let tmiliseconds = time - (tseconds1 * Rounding);
-      let tmiliString = tmiliseconds.toString();
-      let milistr1 = tmiliString.padStart(places, "0");
-      return { tseconds1, milistr1 };
-    }
+// function uptime(inp, msg, cmd) {
+//   function msToTime(time) {
+//     function Reformat(Rounding, places, time) {
+//       let tseconds1 = Math.floor(time / Rounding);
+//       let tmiliseconds = time - (tseconds1 * Rounding);
+//       let tmiliString = tmiliseconds.toString();
+//       let milistr1 = tmiliString.padStart(places, "0");
+//       return { tseconds1, milistr1 };
+//     }
     
-    let { tseconds1:tseconds, milistr1:milistr } = Reformat(1000, 3, time);
-    let { tseconds1:tminuites, milistr1:secstr } = Reformat(60, 1, tseconds);
-    let { tseconds1:thours, milistr1:minstr } = Reformat(60, 1, tminuites);
-    let { tseconds1:tdays, milistr1:minhour } = Reformat(24, 1, thours);
-    var timeList = []
-    var nameList = []
-    for (let i = 0; i < [[minhour, "hour"], [minstr, "min"], [(secstr+"."+milistr), ("sec.MS")]].length; i++) {
-      const thing = [[minhour, "hour"], [minstr, "min"], [(secstr+"."+milistr), ("sec.MS")]][i];
-      if (thing[0] != 0) {
-        timeList.push(thing[0])
-        nameList.push(thing[1])
-      }
-    }
-    if (tdays == 0) {
-      return (timeList.join(":")+" ("+nameList.join(":")+")")
-    } else {
-      return (tdays.toString()+" days and "+timeList.join(":")+" ("+nameList.join(":")+")")
-    }
-  }
-  msg.reply("i have been up for "+msToTime(bot.uptime))
-}
-/**
- * @param {String} inp 
- * @param {Discord.Message} msg 
- * @param {Cmd} cmd 
- */
-function addRule(inp, msg, cmd) {
-  MongoClient.connect(url, function(err, client) {
-    // Create a collection we want to drop later
-    client.db(dbName).collection("rules").countDocuments().then((index)=>{
-      client.db(dbName).collection("rules").insertOne({"rule":"yes", "content":inp.trim().slice(3).trim(), "index":index})
-    })
-  });
-}
-function listRule(inp, msg, cmd) {
-  MongoClient.connect(url, function(err, client) {
-    // Create a collection we want to drop later
-    let value = client.db(dbName).collection("rules").find({"rule":"yes"})
-    value.count().then((count) => {
-      var reply =  count.toString() + " rules\n"
-      value.forEach((doc) => {
-        reply = reply + doc.content + "\n"
-      })
-      // reply = reply.slice(0,2)
-      msg.reply(reply)
-    })
-  });
-}
-let Commands = new Cmd("!", 0, ()=>{}, "", [
-  new Cmd("ping", 0, ping),
-  new Cmd("rule", 1, help, null, [
-    new Cmd("add", 0, addRule),
-    new Cmd("list", 0, listRule)
-  ]),
-  new Cmd("uptime", 2, uptime, "current time online")
-])
-bot.on('message', msg => {
-  // if (msg.channel.name.startsWith("bot")) {
-    Commands.test(msg.content, msg)
-  // }
-  if (msg.content == "hi") {
-    msg.reply("hey!")
-  }
-});
+//     let { tseconds1:tseconds, milistr1:milistr } = Reformat(1000, 3, time);
+//     let { tseconds1:tminuites, milistr1:secstr } = Reformat(60, 1, tseconds);
+//     let { tseconds1:thours, milistr1:minstr } = Reformat(60, 1, tminuites);
+//     let { tseconds1:tdays, milistr1:minhour } = Reformat(24, 1, thours);
+//     var timeList = []
+//     var nameList = []
+//     for (let i = 0; i < [[minhour, "hour"], [minstr, "min"], [(secstr+"."+milistr), ("sec.MS")]].length; i++) {
+//       const thing = [[minhour, "hour"], [minstr, "min"], [(secstr+"."+milistr), ("sec.MS")]][i];
+//       if (thing[0] != 0) {
+//         timeList.push(thing[0])
+//         nameList.push(thing[1])
+//       }
+//     }
+//     if (tdays == 0) {
+//       return (timeList.join(":")+" ("+nameList.join(":")+")")
+//     } else {
+//       return (tdays.toString()+" days and "+timeList.join(":")+" ("+nameList.join(":")+")")
+//     }
+//   }
+//   msg.reply("i have been up for "+msToTime(bot.uptime))
+// }
