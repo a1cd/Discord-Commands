52416ef7b95c677385f6887d7a8e40d535ac2a2f
a1cd <71281043+a1cd@users.noreply.github.com>
2021-06-18 15:08:41 -0400

    changed test stuff

--- corde.config.js
+++ corde.config.js
@@ -1,5 +1,5 @@
 const env = require("dotenv");
-const { testFilesDir, testTimeOut } = require("./config.json");
+const { testFilesDir, testTimeOut } = require("./Tconfig.json");
 
 const result = env.config();
 
@@ -12,18 +12,20 @@ const botPrefix = process.env.PREFIX;
 const botTestId = process.env.BOT_TEST_ID;
 const channelId = process.env.CHANNEL_ID;
 const cordeBotToken = process.env.CORDE_BOT_TOKEN;
-const guildId = process.env.GUILD_ID;
 const testMatches = [testFilesDir];
+const guildId = process.env.GUILD_ID;
 const botToken = process.env.BOT_TOKEN;
 const timeOut = testTimeOut;
-
+/** @type {
+  import('corde/lib/src/types').IConfigOptions
+} */
 module.exports = {
   botPrefix,
   botTestId,
   channelId,
   cordeBotToken,
+  testMatches,
   guildId,
   botToken,
-  testMatches,
   timeOut,
 };
